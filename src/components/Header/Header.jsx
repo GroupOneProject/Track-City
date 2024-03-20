53092522ee76b9497e564dab6763f82786b874b7
Chersilnt Ntervisi <c.ntervisi@outlook.com>
2024-03-20 09:51:08 +0000

    positioned links left and right of the logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,30 +4,31 @@ import logo from '/src/assets/Designer-1.png';
 import './Header.css';
 
 const Header = () => {
-  return (
+ return (
     <header className='header'>
       <div className="header-container">
-        <div className="logo">
-          <Link to="/"><img src={logo} alt="Logo" width="70"/></Link>
-        </div>
-        <div className="site-title">
-          <h1>Track City</h1>
-        </div>
         <div className="nav-container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <ul>
-            <li>
-              <Link to="/how-to-use">How to Use</Link>
-            </li>
-            <li>
-              <Link to="/charts">Charts</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+          <nav className="navbar">
+            <ul>
+              <li>
+                <Link to="/how-to-use">How to Use</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="logo">
+            <Link to="/"><img src={logo} alt="Logo" width="70"/></Link>
+          </div>
+          <nav className="navbar">
+            <ul>
+              <li>
+                <Link to="/charts">Charts</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
-  );
+ );
 }
 
 export default Header;
