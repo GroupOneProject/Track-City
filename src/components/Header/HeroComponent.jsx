81991f65e42ca72efba95baa2dd175593d221ce6
Chersilnt Ntervisi <c.ntervisi@outlook.com>
2024-03-20 14:57:11 +0000

    fixed nav for smaller screens and levelled the buttons with logo

--- src/components/Header/HeroComponent.jsx
+++ src/components/Header/HeroComponent.jsx
@@ -3,7 +3,7 @@ import './HeroComponent.css'
 
 const HeroComponent = () => {
     return (
-      <div className="container-fluid mt-5 hero">
+      <div className="container-fluid mt-1 hero">
         <p className='hero-para'>
         Track City is a versatile application designed to help users track various aspects of their lives, interests, and activities effortlessly. Whether you're monitoring your fitness progress, managing personal finances, or tracking daily habits, Track City empowers you to gather and analyze data in a simple and intuitive way.
         </p>
